ab18a17039875899c03af3dfce0f88c18d151150
djsamyak <djsamyak@gmail.com>
2023-03-11 05:08:52 -0600

    stashing:

--- main_invoker.py
+++ main_invoker.py
@@ -1,10 +1,18 @@
+import os
 import app as backend
 
 offset_counter = 0
-playlist_tracks = []
-increment_counter = 0
+# playlist_uri = "spotify:playlist:74fyd8F4UMEzIXF5dlrWbV"
+# SAVE_PATH = r"D:\Work\Programming\spotify-downloader\media"
+# FFMPEG_PATH = r"D:\Work\Programming\spotify-downloader\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe"
 
-playlist_uri = "spotify:playlist:74fyd8F4UMEzIXF5dlrWbV"
+SPOTIPY_CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
 
+playlist_uri = input("Enter Playlist URI\n")
+SAVE_PATH = input("Enter Download Directory (Absolute Path)\n")
+FFMPEG_PATH = input("Enter Absolute Path for FFMPEG executable\n")
+
+backend.importer([SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,SAVE_PATH,FFMPEG_PATH])
 backend.fetch_playlist_details(playlist_uri,offset_counter)
 backend.download()
